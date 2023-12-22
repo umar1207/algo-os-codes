3ff42946681ab4024b2e8e0bc19daeea2972e63a
umar <mdumarkhan.1207@gmail.com>
2023-12-23 01:01:52 +0530

    minor changes
    minor changes

--- djikstraAlgo/Main.java
+++ djikstraAlgo/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    void printIntermediate(int shortestPath[], int V)
+    static void printIntermediate(int shortestPath[], int V)
     {
         int a = -1;
         System.out.println();
@@ -12,7 +12,7 @@ public class Main {
             else System.out.printf("%3d", a);
         }
     }
-    void printDistance(int shortestPath[], int source, int V)
+    static void printDistance(int shortestPath[], int source, int V)
     {
         System.out.println("\nVertex\t" + "Shortest Path from Source" );
         for(int i=0; i<V; i++)
@@ -21,7 +21,7 @@ public class Main {
         }
     }
 
-    int currentVertex(int shortestPath[], Boolean permVertex[], int v)
+    static int currentVertex(int shortestPath[], Boolean permVertex[], int v)
     {
         int minimum = (int)1e7, index = -1;
         for(int i=0; i<v; i++)
@@ -34,13 +34,13 @@ public class Main {
         }
         return index;
     }
-    void Dijkstra(int weight[][], int source, int V)
+    static void Dijkstra(int weight[][], int source, int V)
     {
         int shortestPath[] = new int[V];
         Boolean permVertex[] = new Boolean[V];
 
-//        initialise shortest path , source ->0 & other -> infinite
-//        set all vertices initially as unprocessed ie permVertex -> false
+        // initialise shortest path , source ->0 & other -> infinite
+        // set all vertices initially as unprocessed ie permVertex -> false
         for(int i=0; i<V; i++)
         {
             shortestPath[i] = (int)1e7;
@@ -57,7 +57,7 @@ public class Main {
         for(int it=0; it<V-1; it++)
         {
             int u = currentVertex(shortestPath, permVertex, V);
-//            u is marked as permanent vertex since now it will be processed
+            // u is marked as permanent vertex since now it will be processed
             permVertex[u] = true;
 
             for(int v=0; v<V; v++)
@@ -68,7 +68,7 @@ public class Main {
             printIntermediate(shortestPath, V);
 
         }
-//        printDistance(shortestPath, source, V);
+        // printDistance(shortestPath, source, V);
     }
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
@@ -79,10 +79,10 @@ public class Main {
         V = inp.nextInt();
         E = inp.nextInt();
 
-//        declaring the graph
+        // declaring the graph
         int [][] graph = new int[V][V];
 
-//        initialising the graph
+        // initialising the graph
         System.out.println("Initialise the graph");
 
         for(int i=0; i<V; i++){
@@ -100,14 +100,13 @@ public class Main {
             graph[u][v] = w;
             graph[v][u] = w;
         }
-//        graph initialised
+        // graph initialised
 
 
         for(int i=0; i<V; i++)
         {
             System.out.println("\n------------------------------------------------");
-            Main t = new Main();
-            t.Dijkstra(graph, i, V);
+            Dijkstra(graph, i, V);
         }
         System.out.println("\n------------------------------------------------");
         inp.close();
